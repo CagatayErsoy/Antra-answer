7c5918b416fea7edfe558fbe1f36f188de054002
Latecoder-hub <cagatay.ersoy1@gmail.com>
2022-07-11 22:00:30 -0700

    updated template

--- index.js
+++ index.js
@@ -1,80 +1,111 @@
-
+/* ~~~~~~~~~~~~~~~~ Api ~~~~~~~~~~~~~~~~ */
+const Api = (() => {
+    
+    const baseUrl = 'url';
+    const path = 'path';
+    // get data from server
+    const getSomething = () =>
+      fetch([baseUrl, path].join('/')).then((response) => response.json());
+    // delete data from database
+    const deleteSomething= (id) =>
+      fetch([baseUrl, path, id].join('/'), {
+        method: 'DELETE',
+      });
+    //add data to database
+    const addSomething = (thing) =>
+      fetch([baseUrl, path].join('/'), {
+        method: 'POST',
+        body: JSON.stringify(thing),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then((response) => response.json());
+  
+    return {
+      getSomething ,
+      deleteSomething,
+      addSomething
+    };
+  })();
 
 //===================View======================================= 
 const View = (() => {
+    // dom elements 
     const domstr ={
-        container1:"#mvc_container1",
-        deleteButton:"deleteButton",
-        firstName:"firstName",
-        lastName:"lastName",
-        email:"email",
-        phone:"phone",
-        submit:"submit",
-       form:"form"
+        container:"#mvc_container1",
+        
     }
+    // render function
     const render=(element, value)=>{
         element.innerHTML = value
 
     }
-    const createPersonTmp=(obj)=>{
+    // template callback function for createTemplate
+    const createObjTmp=(obj)=>{
 
 
-        return `<div>First Name: ${obj.first_name}</div><br>
-                <div>Last Name: ${obj.last_name}</div><br>
-                <div>Phone Number ${obj.phone}</div><br>
-                <div>Email: ${obj.email}</div><br>
-            `
+        // return `<div>First Name: ${obj.first_name}</div><br>
+        //         <div>Last Name: ${obj.last_name}</div><br>
+        //         <div>Phone Number ${obj.phone}</div><br>
+        //         <div>Email: ${obj.email}</div><br>
+        //     `
         
     }
-    const createSection=(arr,callback)=>{
-        let section =""
+    // Create template for Dom
+    const createTemplate=(arr,callback)=>{
+        // Dont forget all data and forEach here, dont forget return
+        // let section =""
        
-        arr.forEach(element=>{
-            section+=` <li>
-            <span> ${element.id}-${callback(element)} </span>
-             <button class="deleteButton" id=${element.id}></button>
-            </li>`
+        // arr.forEach(element=>{
+        //     section+=` <li>
+        //     <span> ${element.id}-${callback(element)} </span>
+        //      <button class="deleteButton" id=${element.id}></button>
+        //     </li>`
            
             
-        })
-        return section
+        // })
+        // return section
 
     }
     return{
         domstr,
         render,
-        createSection,
-        createPersonTmp,
+        createTemplate,
+        createObjTmp,
     }
 })()
 
 
 //===================Model====================================
 const Model = ((api,view) => {
-    
+    // crete constructor class for state management
+
     class User{
-        constructor(firstName, lastName, email,phone){
-            this.firstName=firstName,
-            this.lastName=lastName,
-            this.email=email,
-            this.phone=phone
-        }
+        // constructor(firstName, lastName, email,phone){
+        //     this.firstName=firstName,
+        //     this.lastName=lastName,
+        //     this.email=email,
+        //     this.phone=phone
+        // }
     }
+    // create state for state management
     class State{
-        allUsers=[]
+        // allUsers=[]
     
-
-        get users(){
+        // // getter
+        // get users(){
             
-            return this.allUsers
-        }
-        set users(newUserList){
-            this.allUsers=[...newUserList]
+        //     return this.allUsers
+        // }
+        // //setter
+        // set users(newUserList){
+        //     this.allUsers=[...newUserList]
 
-            const domElement=document.querySelector(view.domstr.container1)
-            const container1=view.createSection(this.allUsers, view.createPersonTmp)
+        //     const domElement=document.querySelector(view.domstr.container1)
+        //     const container1=view.createTemplate(this.allUsers, view.createObjTmp)
            
-            view.render(domElement,container1)
+        //     view.render(domElement,container1)
             
           
                 
@@ -82,15 +113,17 @@ const Model = ((api,view) => {
             
     
 
-        }
+        // }
     }
 
-    const { getUsers,addUser,deleteUser } = api;
+    const { getSomething ,
+        deleteSomething,
+        addSomething } = api;
 
     return {
-        getUsers,
-        addUser,
-        deleteUser,
+        getSomething ,
+        deleteSomething,
+        addSomething,
         State,
         User,
     };
@@ -99,52 +132,48 @@ const Model = ((api,view) => {
 
 //=================== Controller==========================
 const Controller = ((model,view) => {
-   const state=new model.State()
-   const deleteUser=()=>{
-    const userContainer=document.querySelector(view.domstr.container1)
-    userContainer.addEventListener('click',(event)=>{
+   // state
+    const state=new model.State()
+
+    // delete function 
+//    const deleteUser=()=>{
+//     const userContainer=document.querySelector(view.domstr.container1)
+//     userContainer.addEventListener('click',(event)=>{
         
-        if(event.target.className===view.domstr.deleteButton){
+//         if(event.target.className===view.domstr.deleteButton){
             
-            state.users=state.allUsers.filter(user=>
-                +user.id!==+event.target.id
-            )
+//             state.users=state.allUsers.filter(user=>
+//                 +user.id!==+event.target.id
+//             )
             
-        }
-        model.deleteUser(event.target.id)
-    })
+//         }
+//// send api for fetch data
+//         model.deleteUser(event.target.id)
+//     })
 
-   }
-
-   const addUser=()=>{
-    const form=document.getElementById(view.domstr.form)
-    const submit=document.getElementById(view.domstr.submit)
-    const firstName=document.getElementById(view.domstr.firstName)
-    const lastName=document.getElementById(view.domstr.lastName)
-    const email=document.getElementById(view.domstr.email)
-    const phone=document.getElementById(view.domstr.phone)
-    submit.addEventListener("click",(e)=>{
+//    }
+// delete function to Dom
+//    const addUser=()=>{
+  
+//     const submit=document.getElementById(view.domstr.submit)
+//     const firstName=document.getElementById(view.domstr.firstName)
+//     const lastName=document.getElementById(view.domstr.lastName)
+//     const email=document.getElementById(view.domstr.email)
+//     const phone=document.getElementById(view.domstr.phone)
+//     submit.addEventListener("click",(e)=>{
         
-        e.preventDefault()
-        const newUser=new model.User(firstName.value, lastName.value ,email.value, phone.value)
-        console.log(newUser)
-         model.addUser({
-            first_name: "Valaria",
-             last_name: "Luxton",
-            email: "vluxton1@cbslocal.com",
-            phone: "556-998-6327"
-         }).then(user=>{
-         state.users=[user,...state.allUsers]
-     })
+//         const newUser=new model.User(firstName.value, lastName.value ,email.value, phone.value)
+//          model.addUser(newUser).then(user=>{
+//          state.users=[user,...state.allUsers]
+//      })
        
-       // reset part
-       
+//        // reset part
         
 
-    })
+//     })
 
         
-   }
+//    }
    
    
     const init = () => {
